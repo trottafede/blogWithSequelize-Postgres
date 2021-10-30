06815da8752e0f2eae38147707d098e1f61ea717
trottafede <ftrotta18@gmail.com>
2021-10-29 22:16:40 -0300

    Break point de bootstrap

--- models/index.js
+++ models/index.js
@@ -14,6 +14,7 @@ const sequelize = new Sequelize(
     dialect: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
+    logging: false, //Disable sequelize from loggin to the CLI
   }
 );
 
