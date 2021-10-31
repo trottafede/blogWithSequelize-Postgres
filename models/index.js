b5c0a514778f0fd7c4c7d4a9f5475f7de00397a4
trottafede <ftrotta18@gmail.com>
2021-10-31 20:43:04 -0300

    dialect con postgres hardcodeado

--- models/index.js
+++ models/index.js
@@ -10,7 +10,7 @@ const sequelize = new Sequelize(
   process.env.DB_DATABASE,
   process.env.DB_PASSWORD,
   {
-    dialect: process.env.DB_DATABASE,
+    dialect: "postgres",
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     logging: false, //Disable sequelize from loggin to the CLI
